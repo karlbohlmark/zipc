40f73d19828faef12096b867ae084176317ee92e
Karl Böhlmark <karl.bohlmark@netinsight.net>
2025-04-04 20:54:24 +0200

    macos shm path

--- src/main.zig
+++ src/main.zig
@@ -26,12 +26,12 @@ test "simple test" {
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
 
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+test "shm_open" {
+    const path: [*:0]const u8 = "/tmp/my-zipc";
+    const flags: std.posix.O = .{
+        .CREAT = true,
+        .ACCMODE = .RDWR,
     };
-    try std.testing.fuzz(global.testOne, .{});
+    const fd = std.c.shm_open(path, @bitCast(flags), 0o600);
+    try std.testing.expect(fd != -1);
 }
--- src/os.zig
+++ src/os.zig
@@ -3,7 +3,12 @@ const builtin = @import("builtin");
 
 pub fn shm_open(allocator: std.mem.Allocator, name: []const u8, flags: std.posix.O, mode: u16) std.posix.fd_t {
     std.debug.print("shm_open call for name {s}\n", .{name});
-    const dir = "/dev/shm/";
+    const dir = switch (builtin.target.os.tag) {
+        .linux => "/dev/shm/",
+        .macos => "/tmp/",
+        else => std.debug.panic("shm_open not implemented for this OS"),
+    };
+
     // Check that the name starts with a slash ('/') as required by POSIX
     if (name.len == 0 or name[0] != '/') {
         std.debug.print("name len: {} name {s}", .{ name.len, name });
@@ -12,7 +17,8 @@ pub fn shm_open(allocator: std.mem.Allocator, name: []const u8, flags: std.posix
     const full_path = std.mem.concat(allocator, u8, &[_][]const u8{ dir, name[1..], "\x00"[0..1] }) catch {
         std.process.exit(1);
     };
-    // std.debug.print("len2: {}, full path 2 {s} final char2: {}\n", .{ full_path.len, full_path, full_path[full_path.len - 1] });
+
+    std.debug.print("len2: {}, full path 2 {s} final char2: {}\n", .{ full_path.len, full_path, full_path[full_path.len - 1] });
     defer allocator.free(full_path);
     const path_ptr: [*:0]const u8 = @ptrCast(full_path.ptr);
     // Open or create the shared memory object
@@ -23,6 +29,11 @@ pub fn shm_open(allocator: std.mem.Allocator, name: []const u8, flags: std.posix
             return @intCast(fd);
         },
         .macos => {
+            for (full_path, 0..full_path.len) |d, index| {
+                if (d == '-') {
+                    full_path[index] = '_';
+                }
+            }
             const fd = std.c.shm_open(path_ptr, @bitCast(flags), mode);
             std.debug.assert(fd != -1);
             return @intCast(fd);
